0ac6edb6fe5b19172e91fe9ff3cca890528814ee
Josh Szaflarski <jszaflarskidev@outlook.com>
2019-04-20 02:09:02 -0400

    Refactor existing code, Add loop to play again

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -2,47 +2,69 @@
 #include <string>
 
 //function decleration
-void PrintIntro(int WordLength);
+void PrintIntro();
+void PlayGame();
 std::string GetGuess();
 void RepeatGuess(std::string Guess);
+bool AskToPlayAgain();
 
 int main()
 {
-	constexpr int GAME_CYCLE = 5;
-	constexpr int WORD_LENGTH = 9;
-
-	PrintIntro(WORD_LENGTH);
-
-	// loop for number of turns for guesses
-	for (size_t i = 1; i < GAME_CYCLE; i++)
+	//loop to replay game
+	do
 	{
-		RepeatGuess(GetGuess());
-	}
+		PrintIntro();
+		PlayGame();
+	} while (AskToPlayAgain() == true);
 	
-	return 0;
+	return 0; // exit program
 } // end main
 
-void PrintIntro(int WordLength)
+void PrintIntro()
 {
+	constexpr int WORD_LENGTH = 9;
+
 	// introduce game
-	std::cout << "Welcome to Bulls and Cows, a fun word game!" << std::endl;
-	std::cout << "Can you guess the " << WordLength <<
-		" letter isogram I am thinking of?" << std::endl << std::endl;
-} // end PrintIntro
+	std::cout << "Welcome to Bulls and Cows, a fun word game!\n";
+	std::cout << "Can you guess the " << WORD_LENGTH <<
+		" letter isogram I am thinking of?\n\n";
+} // end func
 
 std::string GetGuess()
 {
 	std::string Guess;
 
 	// get guess from user
-	std::cout << "What is your guess?" << std::endl;
+	std::cout << "What is your guess?\n";
 	getline(std::cin, Guess);
 
 	return Guess;
-} // end GetGuess
+} // end func
 
 void RepeatGuess(std::string Guess)
 {
 	// repeat guess from user
 	std::cout << "Your guess is: " << Guess << std::endl <<std::endl;
-} // end RepeatGuess
+} // end func
+
+bool AskToPlayAgain()
+{
+	std::string Response = "";
+
+	std::cout << "Would you like to play again? (y/n)\n";
+	getline(std::cin, Response);
+	std::cout << std::endl << std::endl;
+
+	return (Response[0] == 'y' || Response[0] == 'Y');
+}
+
+void PlayGame()
+{
+	constexpr int GAME_CYCLE = 5;
+
+	// loop for number of turns for guesses
+	for (size_t i = 1; i <= GAME_CYCLE; i++)
+	{
+		RepeatGuess(GetGuess());
+	} //end forloop
+} //end func
